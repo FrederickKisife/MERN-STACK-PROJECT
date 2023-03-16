77b155009c2d91334659fcfde22794dfeda35a1c
FrederickKisife <kilafrederick@gmaul.com>
2023-03-16 14:38:18 -0700

    signup and errors

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,9 +6,9 @@ const {signupUser, loginUser} = require('../controllers/userController')
 const router = express.Router()
 
 // login route
-router.post('/login', loginUser=>{})
+router.post('/login', loginUser)
 
 // signup route
-router.post('/signup', signupUser=>{})
+router.post('/signup', signupUser)
 
 module.exports = router;
