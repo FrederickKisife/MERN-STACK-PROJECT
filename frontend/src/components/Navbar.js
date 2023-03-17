80c49c41406a0476531f6227868be461d7297d5f
FrederickKisife <kilafrederick@gmaul.com>
2023-03-16 17:53:18 -0700

    setting up the user login

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import {Link} from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogout } from '../hooks/useLogout';
+
 
 const Navbar= () => {
     const {logout} = useLogout()
+    const {user} = useAuthContext()
 
     const handleClick = ()=>{
         logout()
@@ -14,14 +17,20 @@ const Navbar= () => {
                 <Link to='/'>
                     <h2>Workout Site</h2>
                 </Link>
+                
                 <nav>
+                {user && (
                     <div>
+                        <span>{user.email}</span>
                         <button onClick={handleClick}>Log out</button>
                     </div>
+                    )}
+                    {!user && (
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
                     </div>
+                    )}
                 </nav>
             </div>
         </header>
